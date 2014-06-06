cb85b03e3967473758b32b4054b178a21bde84d4
Krzysztof Mansz <krzysztof.mansz@gmail.com>
2014-06-06 12:36:08 +0200

    Java Examples from Thinking in Java

---
+++ Java.java
@@ -0,0 +1,8 @@
+//create a reference, not an object
+String s; 
+//always initialize a reference when you create it
+String s = "asdf";
+String s = new String("asdf");
+//wrapper class
+char c= 's';
+Character ch= new Character(c);
