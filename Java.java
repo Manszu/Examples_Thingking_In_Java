9935cbfcdcd8638d97d29e3fdab64751cd691982
Krzysztof Mansz <krzysztof.mansz@gmail.com>
2014-06-06 13:07:19 +0200

    Drugi commit

--- Java.java
+++ Java.java
@@ -6,3 +6,14 @@ String s = new String("asdf");
 //wrapper class
 char c= 's';
 Character ch= new Character(c);
+//Java autoboxing
+Character ch= 'x';
+char c= ch;
+//Scoping
+{ 	int x=12;
+//Only x available
+{	int q=96;
+//Both x&x available
+}
+//only x avalilable
+}
