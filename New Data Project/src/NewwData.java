3c1a939541b42abb95217dcf412ff4fbd804ff41
Krzysztof Mansz <krzysztof.mansz@gmail.com>
2014-06-06 19:49:02 +0200

    new comit

---
+++ New Data Project/src/NewwData.java
@@ -0,0 +1,11 @@
+
+//HelloDate.java
+import java.util.*;
+public class NewwData {
+
+	public static void main(String[] args) {
+		System.out.println("Hello it is");
+		System.out.println(new Date());
+	}
+
+}
